7bc522b4b418c4748c22c1b5c44e4bf5a0a45d02
SqueakyCheese <james_wooltorton@hotmail.com>
2019-06-24 00:49:40 +0100

    fixed locale bug and transactions

--- api/products/business/watchlist.py
+++ api/products/business/watchlist.py
@@ -3,20 +3,22 @@ from api.products.repositories.prices import get_price
 
 
 def get_ticker(ticker):
-    # lookup product
-    product = get_product(ticker)
-    # lookup price
-    price = get_price(ticker)
-    response = {
-        "ccy": product["quote"]["currency"],
-        "change": price["change"],
-        "id": ticker,
-        "movement": price["movement"],
-        "name": product["name"],
-        "price": price["price"],
-        "spot": 0,
-        "symbol": product["quote"]["symbol"],
-        "ticker": ticker
-    }
-
+    try:
+        # lookup product
+        product = get_product(ticker)
+        # lookup price
+        price = get_price(ticker)
+        response = {
+            "ccy": product["quote"]["currency"],
+            "change": price["change"],
+            "id": ticker,
+            "movement": price["movement"],
+            "name": product["name"],
+            "price": price["price"],
+            "spot": 0,
+            "symbol": product["quote"]["symbol"],
+            "ticker": ticker
+        }
+    except:
+        response = None
     return response
