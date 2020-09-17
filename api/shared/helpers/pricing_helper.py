28e0b0caa7f993c4b8de6794274df68bc68e1e16
Jamie Wooltorton <james_wooltorton@hotmail.com>
2020-09-17 12:59:06 +0100

    refactor: Re-factoring holdings

---
+++ api/shared/helpers/pricing_helper.py
@@ -0,0 +1,25 @@
+from api.shared.serialisers import price
+
+
+def calc_movement(increase: float, price: float) -> float:
+    if increase is None or price is None or price is 0:
+        return 0
+    return (increase / price) * 100
+
+
+def calc_total_change(holding: float, change: float) -> float:
+    if holding is None or change is None:
+        return 0
+    return (holding * change)
+
+
+def calc_total(holding: float, price: float) -> float:
+    if holding is None or price is None:
+        return 0
+    return (holding * price)
+
+
+def calc_change(price: float, open: float) -> float:
+    if price is None or open is None:
+        return 0
+    return (price - open)
